397293a42b7c897715089feaa01ba7b60d36022f
mikha-dev <mikha.dev@gmail.com>
2023-11-19 13:13:28 +0300

    boostrap

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -3,14 +3,21 @@
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
 use Dcat\Admin\Admin;
+use Dcat\Admin\DcatIcon;
 use Dcat\Admin\Layout\Footer;
 use Dcat\Admin\Layout\Navbar;
+use Dcat\Admin\Layout\UserNav;
+use Dcat\Admin\Enums\RouteAuth;
+use Dcat\Admin\Layout\ColoredBadge;
 use Illuminate\Support\Facades\App;
+use Dcat\Admin\Enums\StyleClassType;
+use Dcat\Admin\Layout\UserNavElement;
 use Illuminate\Support\Facades\Session;
 use Dcat\Admin\Widgets\Navs\ShortcutsNav;
+use Dcat\Admin\Widgets\Navs\LangSelectorNav;
+use Dcat\Admin\Widgets\Navs\DarkModeSwitcher;
 use Dcat\Admin\Widgets\Navs\DarkModeSwitcherNav;
 use Dcat\Admin\Widgets\Navs\DashboardNotificationNav;
-use Dcat\Admin\Widgets\Navs\LangSelectorNav;
 
 Form::resolving(function($form) {
     $form->disableEditingCheck();
@@ -31,16 +38,18 @@ if(empty($locale))
 if(!empty($locale) && !App::isLocale($locale)) {
     App::setLocale($locale);
 }
+
 if (! Dcat\Admin\Support\Helper::isAjaxRequest()) {
 
     Admin::navbar(function (Navbar $navbar) {
-        //todo::fix
-        // $navbar->start(new LangSelectorNav());
-        // $navbar->end(new ShortcutsNav( function (ShortcutsNav $shortcuts) {
-        //     $shortcuts->add('calendar', 'Calendar', 'Appointments', admin_url('/'));
-        // }));
-        // $navbar->end(new DarkModeSwitcherNav());
-        // $navbar->end(new DashboardNotificationNav());
+        $navbar->putFree(new DarkModeSwitcher());
+
+        $navbar->putNav(new LangSelectorNav());
+        $navbar->putNav(new ShortcutsNav( function (ShortcutsNav $shortcuts) {
+            $shortcuts->add(DcatIcon::CALENDAR(), __('admin.calendar'), __('admin.appointments'), admin_route(RouteAuth::HOME()));
+        }));
+        $navbar->putNav(new DarkModeSwitcherNav());
+        $navbar->putNav(new DashboardNotificationNav());
     });
 
     Admin::footer(function (Footer $footer) {
@@ -48,4 +57,7 @@ if (! Dcat\Admin\Support\Helper::isAjaxRequest()) {
         $footer->end('test_end');
     });
 
+    Admin::userNav(function (UserNav $userNav) {
+        $userNav->put(new UserNavElement($userNav, admin_route(RouteAuth::HOME()), DcatIcon::HOME(), __('admin.home'), new ColoredBadge(5, StyleClassType::DANGER), true));
+    });
 }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -14,95 +14,95 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
 
-    $router->resource('example', 'ExampleController');
+    // $router->resource('example', 'ExampleController');
 
-    $router->resource('clients', 'UserController');
+    // $router->resource('clients', 'UserController');
 
-    // 布局示例
-    $router->get('layout', 'LayoutController@index');
-    // 报表示例
-    $router->get('reports', 'ReportController@index');
-    $router->get('reports/preview', 'ReportController@preview');
-    // 固定列功能示例
-    $router->get('fixed-columns', 'FixedController@index');
-    $router->get('fixed-columns/preview', 'FixedController@preview');
-    // 固定列功能示例
-    $router->get('with-border', 'BorderTableController@index');
-    $router->get('with-border/preview', 'BorderTableController@preview');
-    // 行间距
-    $router->get('row-space', 'RowSpaceController@index');
-    $router->get('row-space/preview', 'RowSpaceController@preview');
-    // 自定义表格视图
-    $router->get('grid/custom', 'CustomGridController@index');
-    $router->get('grid/custom/preview', 'CustomGridController@preview');
+    // // 布局示例
+    // $router->get('layout', 'LayoutController@index');
+    // // 报表示例
+    // $router->get('reports', 'ReportController@index');
+    // $router->get('reports/preview', 'ReportController@preview');
+    // // 固定列功能示例
+    // $router->get('fixed-columns', 'FixedController@index');
+    // $router->get('fixed-columns/preview', 'FixedController@preview');
+    // // 固定列功能示例
+    // $router->get('with-border', 'BorderTableController@index');
+    // $router->get('with-border/preview', 'BorderTableController@preview');
+    // // 行间距
+    // $router->get('row-space', 'RowSpaceController@index');
+    // $router->get('row-space/preview', 'RowSpaceController@preview');
+    // // 自定义表格视图
+    // $router->get('grid/custom', 'CustomGridController@index');
+    // $router->get('grid/custom/preview', 'CustomGridController@preview');
 
-    // simple page
-    $router->get('full', 'FullPageController@index');
+    // // simple page
+    // $router->get('full', 'FullPageController@index');
 
-    $router->get('tree', 'GridTreeController@index');
-    $router->get('tree/preview', 'GridTreeController@preview');
+    // $router->get('tree', 'GridTreeController@index');
+    // $router->get('tree/preview', 'GridTreeController@preview');
 
-    // grid
-    $router->resource('components/grid', 'GridController', ['except' => ['create', 'show', 'destroy']]);
-    $router->get('components/grid/preview', 'GridController@preview');
-    // form
-    $router->get('form', 'FormController@index');
-    $router->post('form', 'FormController@index');
-    $router->get('form/preview', 'FormController@preview');
+    // // grid
+    // $router->resource('components/grid', 'GridController', ['except' => ['create', 'show', 'destroy']]);
+    // $router->get('components/grid/preview', 'GridController@preview');
+    // // form
+    // $router->get('form', 'FormController@index');
+    // $router->post('form', 'FormController@index');
+    // $router->get('form/preview', 'FormController@preview');
 
-    // 分步表单
-    $router->get('form/step/preview', 'StepFormController@preview');
-    $router->get('form/step', 'StepFormController@index');
-    $router->post('form/step', 'StepFormController@store');
+    // // 分步表单
+    // $router->get('form/step/preview', 'StepFormController@preview');
+    // $router->get('form/step', 'StepFormController@index');
+    // $router->post('form/step', 'StepFormController@store');
 
-    // 动态显示表单
-    $router->get('form/when/preview', 'FormWhenController@preview');
-    $router->get('form/when', 'FormWhenController@index');
-    $router->post('form/when', 'FormWhenController@store');
+    // // 动态显示表单
+    // $router->get('form/when/preview', 'FormWhenController@preview');
+    // $router->get('form/when', 'FormWhenController@index');
+    // $router->post('form/when', 'FormWhenController@store');
 
-    // 表单弹窗
-    $router->get('form/modal', 'ModalFormController@index');
-    $router->get('form/modal/preview', 'ModalFormController@preview');
+    // // 表单弹窗
+    // $router->get('form/modal', 'ModalFormController@index');
+    // $router->get('form/modal/preview', 'ModalFormController@preview');
 
-    $router->get('form/tinymce', 'EditorController@tinymce');
-    $router->get('form/tinymce/preview', 'EditorController@preview');
-    $router->get('form/markdown', 'EditorController@markdown');
-    $router->get('form/summercode', 'EditorController@summercode');
+    // $router->get('form/tinymce', 'EditorController@tinymce');
+    // $router->get('form/tinymce/preview', 'EditorController@preview');
+    // $router->get('form/markdown', 'EditorController@markdown');
+    // $router->get('form/summercode', 'EditorController@summercode');
 
-    // 表格
-    $router->get('tables/selector', 'SelectorController@index');
-    $router->get('tables/selector/preview', 'SelectorController@preview');
+    // // 表格
+    // $router->get('tables/selector', 'SelectorController@index');
+    // $router->get('tables/selector/preview', 'SelectorController@preview');
 
 
-    $router->get('components/accordion', 'Components\AccordionController@index');
-    $router->get('components/charts', 'Components\ChartController@index');
-    $router->get('components/charts/preview', 'Components\ChartController@preview');
-    $router->get('components/card-box', 'Components\BoxController@index');
-    $router->get('components/alert', 'Components\AlertController@index');
-    $router->get('components/tab-button', 'Components\TabController@index');
-    $router->get('components/markdown', 'Components\MarkdownController@index');
-    $router->get('components/layer', 'Components\LayerController@index');
-    $router->get('components/checkbox-radio', 'Components\CheckboxController@index');
-    $router->get('components/checkbox-radio/preview', 'Components\CheckboxController@preview');
-    $router->get('components/tooltip', 'Components\TooltipController@index');
-    $router->get('components/dropdown-menu', 'Components\DropdownMenuController@index');
-    $router->get('components/loading', 'Components\LoadingController@index');
-    $router->get('components/loading/preview', 'Components\LoadingController@preview');
-    $router->get('components/metric-cards', 'Components\MetricCardController@index');
-    $router->get('components/metric-cards/preview', 'Components\MetricCardController@preview');
+    // $router->get('components/accordion', 'Components\AccordionController@index');
+    // $router->get('components/charts', 'Components\ChartController@index');
+    // $router->get('components/charts/preview', 'Components\ChartController@preview');
+    // $router->get('components/card-box', 'Components\BoxController@index');
+    // $router->get('components/alert', 'Components\AlertController@index');
+    // $router->get('components/tab-button', 'Components\TabController@index');
+    // $router->get('components/markdown', 'Components\MarkdownController@index');
+    // $router->get('components/layer', 'Components\LayerController@index');
+    // $router->get('components/checkbox-radio', 'Components\CheckboxController@index');
+    // $router->get('components/checkbox-radio/preview', 'Components\CheckboxController@preview');
+    // $router->get('components/tooltip', 'Components\TooltipController@index');
+    // $router->get('components/dropdown-menu', 'Components\DropdownMenuController@index');
+    // $router->get('components/loading', 'Components\LoadingController@index');
+    // $router->get('components/loading/preview', 'Components\LoadingController@preview');
+    // $router->get('components/metric-cards', 'Components\MetricCardController@index');
+    // $router->get('components/metric-cards/preview', 'Components\MetricCardController@preview');
 
-    $router->get('components/modal', 'Components\ModalController@index');
-    $router->get('components/modal/preview', 'Components\ModalController@preview');
+    // $router->get('components/modal', 'Components\ModalController@index');
+    // $router->get('components/modal/preview', 'Components\ModalController@preview');
 
-    // movies
-    $router->get('movies/coming-soon', 'Movies\ComingSoonController@index');
-    $router->get('movies/coming-soon/preview', 'Movies\ComingSoonController@preview');
-    $router->resource('movies/in-theaters', 'Movies\InTheaterController', ['except' => ['create', 'show']]);
-    $router->get('movies/in-theaters/preview', 'Movies\InTheaterController@preview');
-    $router->get('movies/top250', 'Movies\Top250Controller@index');
-    $router->get('movies/top250/preview', 'Movies\Top250Controller@preview');
+    // // movies
+    // $router->get('movies/coming-soon', 'Movies\ComingSoonController@index');
+    // $router->get('movies/coming-soon/preview', 'Movies\ComingSoonController@preview');
+    // $router->resource('movies/in-theaters', 'Movies\InTheaterController', ['except' => ['create', 'show']]);
+    // $router->get('movies/in-theaters/preview', 'Movies\InTheaterController@preview');
+    // $router->get('movies/top250', 'Movies\Top250Controller@index');
+    // $router->get('movies/top250/preview', 'Movies\Top250Controller@preview');
 
-    $router->get('/extensions/ueditor', 'Extensions\UeditorController@index');
-    $router->get('/extensions/ueditor/preview', 'Extensions\UeditorController@preview');
+    // $router->get('/extensions/ueditor', 'Extensions\UeditorController@index');
+    // $router->get('/extensions/ueditor/preview', 'Extensions\UeditorController@preview');
 
 });
