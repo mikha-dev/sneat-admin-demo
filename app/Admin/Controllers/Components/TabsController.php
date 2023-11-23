e8fe6f93ab59af311e8ca570817820e0572e9ba1
Arvin Rahnama <arvin.rnm@gmail.com>
2023-11-23 21:12:31 +0330

    Update TabsController.php

--- app/Admin/Controllers/Components/TabsController.php
+++ app/Admin/Controllers/Components/TabsController.php
@@ -1,56 +1,51 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Admin\Controllers\Components;
 
+use Dcat\Admin\DcatIcon;
 use Faker\Factory;
 use Dcat\Admin\Layout\Row;
-use Dcat\Admin\Widgets\Card;
 use Dcat\Admin\Widgets\Code;
 use Dcat\Admin\Layout\Content;
 use Illuminate\Routing\Controller;
 use Dcat\Admin\Widgets\Tab;
 
-class TabsController extends Controller
-{
-    public function index(Content $content)
-    {
-        return $content->header('Tabs')
-            ->row(function (Row $row) {
-                $faker = Factory::create();
-
-                $tabs1 = new Tab();
-                $tabs1->add($faker->text(20), $faker->text(200), true, 'first');
-                $tabs1->add($faker->text(20), $faker->text(200), false, 'second');
-                $tabs1->add($faker->text(20), $faker->text(200), false, 'third');
-
-                $row->column(6, new Card('Basic', $tabs1));
-
-                $tabs2 = new Tab();
-                $tabs2->add($faker->text(20), $faker->text(200), true, 'first');
-                $tabs2->add($faker->text(20), $faker->text(200), false, 'second');
-                $tabs2->add($faker->text(20), $faker->text(200), false, 'third');
-
-                $row->column(6, new Card('Filled Tab', $tabs2));
-            })
-            ->row(function (Row $row) {
-                $faker = Factory::create();
-
-                $tabs1 = new Tab();
-                $tabs1->add($faker->text(20), $faker->text(200), true, 'first');
-                $tabs1->add($faker->text(20), $faker->text(200), false, 'second');
-                $tabs1->add($faker->text(20), $faker->text(200), false, 'third');
-
-                $row->column(6, new Card('Pills Basic', $tabs1));
-
-                $tabs2 = new Tab();
-                $tabs2->add($faker->text(20), $faker->text(200), true, 'first');
-                $tabs2->add($faker->text(20), $faker->text(200), false, 'second');
-                $tabs2->add($faker->text(20), $faker->text(200), false, 'third');
-
-                $row->column(6, new Card('Pills Filled', $tabs2));
-            })
-
-            ->row(new Card('Code', new Code(__FILE__, 15, 44)));
-
-    }
+class TabsController extends Controller {
+	public function index(Content $content) {
+		return $content->header('Tabs')
+			->row(function (Row $row) {
+				$faker = Factory::create();
+				$tabs1 = new Tab();
+				$tabs1->title('Tab');
+				$tabs1->add($faker->text(20), $faker->text(200), TRUE);
+				$tabs1->add($faker->text(20), $faker->text(200));
+				$tabs1->add($faker->text(20), $faker->text(200));
+				$row->column(6, $tabs1);
+				$tabs2 = new Tab();
+				$tabs2->title('Fill Tab');
+				$tabs2->fill();
+				$tabs2->add($faker->text(20), $faker->text(200), TRUE, DcatIcon::HOME, 4);
+				$tabs2->add($faker->text(20), $faker->text(200), FALSE, DcatIcon::USER);
+				$tabs2->add($faker->text(20), $faker->text(200), FALSE, DcatIcon::MESSAGE_SQUARE);
+				$row->column(6, $tabs2);
+			})
+			->row(function (Row $row) {
+				$faker = Factory::create();
+				$tabs1 = new Tab(Tab::TYPE_PILL);
+				$tabs1->title('Pill');
+				$tabs1->add($faker->text(20), $faker->text(200), TRUE);
+				$tabs1->add($faker->text(20), $faker->text(200));
+				$tabs1->add($faker->text(20), $faker->text(200));
+				$row->column(6, $tabs1);
+				$tabs2 = new Tab(Tab::TYPE_PILL);
+				$tabs2->title('Pill Fill');
+				$tabs2->fill();
+				$tabs2->add($faker->text(20), $faker->text(200), TRUE, DcatIcon::HOME, 3);
+				$tabs2->add($faker->text(20), $faker->text(200), FALSE, DcatIcon::USER);
+				$tabs2->add($faker->text(20), $faker->text(200), FALSE, DcatIcon::MESSAGE_SQUARE);
+				$row->column(6, $tabs2);
+			})
+			->row(new Code(__FILE__, 15, 44));
+	}
 }
